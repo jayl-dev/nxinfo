ada16735f5db96df20def610e7cac5fa6e2eae21
jayl-dev <jay.liang@outlook.com>
2026-02-19 22:25:57 -0500

    1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jl.nxinfo"
         minSdk = 21
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
